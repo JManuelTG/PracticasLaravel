699c147849accdd0c7e4ba57572eb6401a448824
JManuelTG <jose.tarula@alumnos.udg.mx>
2022-09-09 18:00:35 -0500

    Se corrige error en area de mensaje

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -11,22 +11,21 @@
 	<form action="/recibe_form_contacto" method="POST">
 		@csrf
 		<label for="nombre">Nombre:</label><br>
-		<input type="text" name="nombre" id="nombre">
+		<input type="text" name="nombre" id="nombre" value="{{ old('nombre')}}">
 		@error('nombre')
 			<i>{{ $message }}</i>
 		@enderror
 		<br>
 
 		<label for="Correo">Correo:</label><br>
-		<input type="mail" name="correo" id="correo">
+		<input type="mail" name="correo" id="correo" value="{{ old('correo')}}" >
 		@error('correo')
 			<i>{{ $message }}</i>
 		@enderror
 		<br>
 
 		<label for="mensaje">Mensaje</label><br>
-		<textarea name="mensaje" id="mensaje" cols="50" rows="10">
-		</textarea>
+		<textarea name="mensaje" id="mensaje" cols="50" rows="10" >{{ old('mensaje')}}</textarea>
 		@error('mensaje')
 			<i>{{ $message }}</i>
 		@enderror
