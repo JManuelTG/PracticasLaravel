f06a7e3c6e375d8b4091a7e569033f6c6ad41422
JManuelTG <jose.tarula@alumnos.udg.mx>
2022-09-20 16:48:28 -0500

    Pruebas de insersion a bd con modelos

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Contacto;
 
 class SitioController extends Controller
 {
@@ -22,13 +23,28 @@ class SitioController extends Controller
     public function recibe_form_contacto(Request $request){
         // dd($request->all());
 
-        $request->validate([
-            'nombre' => 'required|max:255|min:3',
-            'correo' => ['required','email'],
-            'mensaje' => 'required',
-        ]);
+        // $request->validate([
+            // 'nombre' => 'required|max:255|min:3',
+            // 'correo' => ['required','email'],
+            // 'mensaje' => 'required',
+        // ]);
 
-        DB::insert('insert into contactos (nombre, correo, mensaje) values (?,?,?)',[$request->nombre,$request->correo,$request->mensaje]);
+        // $contacto = new Contacto();
+        // $contacto->nombre = $request->nombre;
+        // $contacto->correo = $request->correo;
+        // $contacto->mensaje = $request->mensaje;
+        // $contacto->save();
+        
+        // Contacto::create([
+        //     'nombre' => $request->nombre,
+        //     'correo' => $request->correo,
+        //     'mensaje' => $request->mensaje,
+        // ]);
+
+        Contacto::create($request->all());
+
+        #DB::insert('insert into contactos (nombre, correo, mensaje) values (?,?,?)',[$request->nombre,$request->correo,$request->mensaje]);
+        #DB::table('contactos')->insert($request->except('_tokem'));
         return redirect("/landingpage");
     }
 
